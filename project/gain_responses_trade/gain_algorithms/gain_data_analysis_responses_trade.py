4862fee55d3b20c6e8f861d4d9a2fabc12f49f8f
juanhenao21 <juanhenao21@gmail.com>
2020-01-21 16:15:31 +0100

    Modify the analysis and plot functions responses trade

--- project/gain_responses_trade/gain_algorithms/gain_data_analysis_responses_trade.py
+++ project/gain_responses_trade/gain_algorithms/gain_data_analysis_responses_trade.py
@@ -1,18 +1,16 @@
 '''GAIN data analysis module.
 
-The functions in the module extract the bid and ask from the Historic Rate Data
-from GAIN Capital in a year.
+The functions in the module compute the response function in trade time scale
+from the Historic Rate Data from GAIN Capital data in a year.
 
 This script requires the following modules:
     * numpy
     * pandas
 
 The module contains the following functions:
-    * gain_fx_year_extract_data - extracts the bid and ask for a year.
-    * gain_fx_midpoint_year_extract_data - extracts the midpoint price for a
-     year
-    * gain_fx_trade_signs_year_extract_data - extracts the midpoint price for a
-     year
+    * gain_fx_self_response_year_responses_trade - extracts the midpoint price
+     for a year
+    * main - the main function of the script.
 
 ..moduleauthor:: Juan Camilo Henao Londono <www.github.com/juanhenao21>
 '''
@@ -23,99 +21,14 @@ import numpy as np
 import pandas as pd
 import pickle
 
-import gain_data_tools_data_extraction
+import gain_data_tools_responses_trade
+
+__tau__ = 1000
 
 # -----------------------------------------------------------------------------
 
 
-def gain_fx_year_extract_data(fx_pair, year):
-    """Extracts the bid and ask for a year.
-
-    :param fx_pair: string of the abbreviation of the forex pair to be analized
-     (i.e. 'eur_usd').
-    :param year: string of the year to be analized (i.e. '2016').
-    :return: pandas dataframe -- The function returns a pandas dataframe with
-     the data.
-    """
-
-    function_name = gain_fx_year_extract_data.__name__
-    gain_data_tools_data_extraction \
-        .gain_function_header_print_data(function_name, fx_pair, year, '')
-
-    fx_data_col = ['lTid', 'cDealable', 'CurrencyPair', 'RateDateTime',
-                   'RateBid', 'RateAsk']
-    fx_data = pd.DataFrame(columns=fx_data_col)
-
-    for m_num in range(1,13):
-
-        if (m_num < 10):
-            m_num = f'0{m_num}'
-
-        for w_num in range(1,6):
-
-            try:
-                fx_data = fx_data.append(pd.read_csv(
-                    f'../../gain_data/original_data_{year}/{fx_pair}_{year}/'
-                    + f'{fx_pair}_{year}{m_num}_w{w_num}.zip'))
-
-            except FileNotFoundError as e:
-                print('No data')
-                print(e)
-                print()
-
-    fx_data.index = pd.to_datetime(fx_data['RateDateTime'])
-
-    # Saving data
-    gain_data_tools_data_extraction \
-        .gain_save_data(function_name, fx_data, fx_pair, year, '')
-
-    return fx_data
-
-# -----------------------------------------------------------------------------
-
-
-def gain_fx_midpoint_year_extract_data(fx_pair, year):
-    """Extracts the midpoint price for a year.
-
-    :param fx_pair: string of the abbreviation of the forex pair to be analized
-     (i.e. 'eur_usd').
-    :param year: string of the year to be analized (i.e. '2016').
-    :return: tuple -- The function returns a tuple with numpy arrays.
-    """
-
-    function_name = gain_fx_midpoint_year_extract_data.__name__
-    gain_data_tools_data_extraction \
-        .gain_function_header_print_data(function_name, fx_pair, year, '')
-
-    try:
-        # Load data
-        fx_data = pickle.load(open(
-                        f'../../gain_data/data_extraction_{year}/gain_fx_year'
-                        + f'_extract_data/gain_fx_year_extract_data_{year}'
-                        + f'_{fx_pair}.pickle', 'rb'))
-
-        time = fx_data['RateDateTime'].to_numpy()
-        ask = fx_data['RateAsk'].to_numpy()
-        bid = fx_data['RateBid'].to_numpy()
-
-        midpoint = (ask + bid) / 2
-
-        # Saving data
-        gain_data_tools_data_extraction \
-            .gain_save_data(function_name, (time, midpoint), fx_pair, year, '')
-
-        return (time, midpoint)
-
-    except FileNotFoundError as e:
-        print('No data')
-        print(e)
-        print()
-        return None
-
-# -----------------------------------------------------------------------------
-
-
-def gain_fx_trade_signs_year_extract_data(fx_pair, year):
+def gain_fx_self_response_year_responses_second(fx_pair, year):
     """Extracts the trade signs price for a year.
 
     The trade signs are obtained from the midpoint price as
@@ -129,35 +42,61 @@ def gain_fx_trade_signs_year_extract_data(fx_pair, year):
     :return: tuple -- The function returns a tuple with numpy arrays.
     """
 
-    function_name = gain_fx_trade_signs_year_extract_data.__name__
-    gain_data_tools_data_extraction \
+    function_name = gain_fx_self_response_year_responses_second.__name__
+    gain_data_tools_responses_trade \
         .gain_function_header_print_data(function_name, fx_pair, year, '')
 
     try:
         # Load data
-        time, midpoint = pickle.load(open(
+        _, midpoint = pickle.load(open(
                         f'../../gain_data/data_extraction_{year}/gain_fx'
-                        + f'_midpoint_year_extract_data/gain_fx_midpoint_year'
-                        + f'_extract_data_{year}_{fx_pair}.pickle', 'rb'))
+                        + f'_midpoint_year_data_extraction/gain_fx_midpoint'
+                        + f'_year_data_extraction_{year}_{fx_pair}.pickle',
+                        'rb'))
+        _, trade_signs = pickle.load(open(
+                        f'../../gain_data/data_extraction_{year}/gain_fx'
+                        + f'_trade_signs_year_data_extraction/gain_fx_trade'
+                        + f'_signs_year_data_extraction_{year}_{fx_pair}'
+                        + f'.pickle', 'rb'))
 
-        trade_signs = 0 * midpoint
+        # Relate the return of the previous second with the current trade sign
+        midpoint = midpoint[:-1]
+        trade_signs = trade_signs[1:]
 
-        for m_idx, m_val in enumerate(midpoint):
+        assert len(midpoint) == len(trade_signs)
 
-            sign = np.sign(m_val - midpoint[m_idx - 1])
-            if (sign):
-                trade_signs[m_idx] = sign
-            else:
-                trade_signs[m_idx] = trade_signs[m_idx - 1]
+        # Array of the average of each tau
+        self_response_tau = np.zeros(__tau__)
+        num = np.zeros(__tau__)
 
-        assert np.sum(trade_signs == 0) == 0
+        # Calculating the midpoint price return and the self-response function
+        # Depending on the tau value
+        for tau_idx in range(__tau__):
+
+            trade_sign_tau = trade_signs[:-tau_idx - 1]
+            trade_sign_no_0_len = len(trade_sign_tau[trade_sign_tau != 0])
+            num[tau_idx] = trade_sign_no_0_len
+            # Obtain the midpoint price return. Displace the numerator tau
+            # values to the right and compute the return
+
+            # Midpoint price returns
+            log_return_sec = (midpoint[tau_idx + 1:]
+                              - midpoint[:-tau_idx - 1]) \
+                / midpoint[:-tau_idx - 1]
+
+            # Obtain the self response value
+            if (trade_sign_no_0_len != 0):
+                product = log_return_sec * trade_sign_tau
+                self_response_tau[tau_idx] = np.sum(product)
+
+        self_response = self_response_tau / num
 
         # Saving data
-        gain_data_tools_data_extraction \
-            .gain_save_data(function_name, (time, trade_signs), fx_pair, year,
+        gain_data_tools_responses_trade \
+            .gain_save_data(function_name, self_response, fx_pair, year,
                             '')
 
-        return (time, trade_signs)
+        return self_response
 
     except FileNotFoundError as e:
         print('No data')
@@ -176,9 +115,7 @@ def main():
     :return: None.
     """
 
-    _, trae = gain_fx_trade_signs_year_extract_data('eur_usd', '2016')
-    print(trae)
-    print(trae[trae == 0])
+    gain_fx_self_response_year_responses_second('eur_usd', '2016')
 
     return None
 
