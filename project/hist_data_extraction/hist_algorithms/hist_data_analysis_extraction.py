3df1e258096c25db5d53769e3462b9aaffa4dc68
juanhenao21 <juanhenao21@gmail.com>
2020-03-17 09:18:05 +0100

    Add hist data extraction code

---
+++ project/hist_data_extraction/hist_algorithms/hist_data_analysis_extraction.py
@@ -0,0 +1,189 @@
+'''GAIN data analysis module.
+
+The functions in the module extract the bid and ask from the Historic Rate Data
+from GAIN Capital in a year.
+
+This script requires the following modules:
+    * numpy
+    * pandas
+
+The module contains the following functions:
+    * gain_fx_year_data_extraction - extracts the bid and ask for a year.
+    * gain_fx_midpoint_year_data_extraction - extracts the midpoint price for a
+     year
+    * gain_fx_trade_signs_year_data_extraction - extracts the midpoint price
+     for a year
+    * main - the main function of the script.
+
+..moduleauthor:: Juan Camilo Henao Londono <www.github.com/juanhenao21>
+'''
+# -----------------------------------------------------------------------------
+# Modules
+
+import numpy as np
+import pandas as pd
+import pickle
+
+import gain_data_tools_extraction
+
+# -----------------------------------------------------------------------------
+
+
+def gain_fx_year_data_extraction(fx_pair, year):
+    """Extracts the bid and ask for a year.
+
+    :param fx_pair: string of the abbreviation of the forex pair to be analyzed
+     (i.e. 'eur_usd').
+    :param year: string of the year to be analyzed (i.e. '2016').
+    :return: pandas dataframe -- The function returns a pandas dataframe with
+     the data.
+    """
+
+    function_name = gain_fx_year_data_extraction.__name__
+    gain_data_tools_extraction \
+        .gain_function_header_print_data(function_name, fx_pair, year, '')
+
+    fx_data_col = ['lTid', 'cDealable', 'CurrencyPair', 'RateDateTime',
+                   'RateBid', 'RateAsk']
+    fx_data = pd.DataFrame(columns=fx_data_col)
+
+    for m_num in range(1,13):
+
+        if (m_num < 10):
+            m_num = f'0{m_num}'
+
+        for w_num in range(1,6):
+
+            try:
+                fx_data = fx_data.append(pd.read_csv(
+                    f'../../gain_data/original_data_{year}/{fx_pair}_{year}/'
+                    + f'{fx_pair}_{year}{m_num}_w{w_num}.zip'))
+
+            except FileNotFoundError as e:
+                print('No data')
+                print(e)
+                print()
+
+    fx_data.index = pd.to_datetime(fx_data['RateDateTime'])
+
+    # Saving data
+    gain_data_tools_extraction \
+        .gain_save_data(function_name, fx_data, fx_pair, year, '')
+
+    return fx_data
+
+# -----------------------------------------------------------------------------
+
+
+def gain_fx_midpoint_year_data_extraction(fx_pair, year):
+    """Extracts the midpoint price for a year.
+
+    :param fx_pair: string of the abbreviation of the forex pair to be analyzed
+     (i.e. 'eur_usd').
+    :param year: string of the year to be analyzed (i.e. '2016').
+    :return: tuple -- The function returns a tuple with numpy arrays.
+    """
+
+    function_name = gain_fx_midpoint_year_data_extraction.__name__
+    gain_data_tools_extraction \
+        .gain_function_header_print_data(function_name, fx_pair, year, '')
+
+    try:
+        # Load data
+        fx_data = pickle.load(open(
+                        f'../../gain_data/data_extraction_{year}/gain_fx_year'
+                        + f'_data_extraction/gain_fx_year_data_extraction'
+                        + f'_{year}_{fx_pair}.pickle', 'rb'))
+
+        time = fx_data['RateDateTime'].to_numpy()
+        ask = fx_data['RateAsk'].to_numpy()
+        bid = fx_data['RateBid'].to_numpy()
+
+        midpoint = (ask + bid) / 2
+
+        # Saving data
+        gain_data_tools_extraction \
+            .gain_save_data(function_name, (time, midpoint), fx_pair, year, '')
+
+        return (time, midpoint)
+
+    except FileNotFoundError as e:
+        print('No data')
+        print(e)
+        print()
+        return None
+
+# -----------------------------------------------------------------------------
+
+
+def gain_fx_trade_signs_year_data_extraction(fx_pair, year):
+    """Extracts the trade signs price for a year.
+
+    The trade signs are obtained from the midpoint price as
+    $\epsilon(t) = sign(m(t) - m(t - 1))$, where +1 indicates the trade was
+    triggered by a market order to buy, and -1 indicates the trade was
+    triggered by a market order to sell.
+
+    :param fx_pair: string of the abbreviation of the forex pair to be analyzed
+     (i.e. 'eur_usd').
+    :param year: string of the year to be analyzed (i.e. '2016').
+    :return: tuple -- The function returns a tuple with numpy arrays.
+    """
+
+    function_name = gain_fx_trade_signs_year_data_extraction.__name__
+    gain_data_tools_extraction \
+        .gain_function_header_print_data(function_name, fx_pair, year, '')
+
+    try:
+        # Load data
+        time, midpoint = pickle.load(open(
+                        f'../../gain_data/data_extraction_{year}/gain_fx'
+                        + f'_midpoint_year_data_extraction/gain_fx_midpoint'
+                        + f'_year_data_extraction_{year}_{fx_pair}.pickle',
+                        'rb'))
+
+        trade_signs = 0 * midpoint
+
+        for m_idx, m_val in enumerate(midpoint):
+
+            sign = np.sign(m_val - midpoint[m_idx - 1])
+            if (sign):
+                trade_signs[m_idx] = sign
+            else:
+                trade_signs[m_idx] = trade_signs[m_idx - 1]
+
+        assert np.sum(trade_signs == 0) == 0
+
+        # Saving data
+        gain_data_tools_extraction \
+            .gain_save_data(function_name, (time, trade_signs), fx_pair, year,
+                            '')
+
+        return (time, trade_signs)
+
+    except FileNotFoundError as e:
+        print('No data')
+        print(e)
+        print()
+        return None
+
+# -----------------------------------------------------------------------------
+
+
+def main():
+    """The main function of the script.
+
+    The main function is used to test the functions in the script.
+
+    :return: None.
+    """
+
+    pass
+
+    return None
+
+# -----------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    main()
---
+++ project/hist_data_extraction/hist_algorithms/hist_data_main_extraction.py
@@ -0,0 +1,97 @@
+'''Gain data main module.
+
+The functions in the module extract and plot the Historic Rate data from GAIN
+Capital in a year.
+
+This script requires the following modules:
+    * itertools.product
+    * multiprocessing
+    * gain_data_analysis_extraction
+    * gain_data_plot_extraction
+    * gain_data_tools_extraction
+
+The module contains the following functions:
+    * gain_data_plot_generator - generates all the analysis and plots from the
+      GAIN data.
+    * main - the main function of the script.
+
+.. moduleauthor:: Juan Camilo Henao Londono <www.github.com/juanhenao21>
+'''
+
+# -----------------------------------------------------------------------------
+# Modules
+
+from itertools import product as iprod
+import multiprocessing as mp
+
+import gain_data_analysis_extraction
+import gain_data_plot_extraction
+import gain_data_tools_extraction
+
+# -----------------------------------------------------------------------------
+
+
+def gain_data_plot_generator(fx_pairs, year):
+    """Generates all the analysis and plots from the GAIN data.
+
+    :param fx_pairs: list of the string abbreviation of the forex pairs to be
+     analyzed (i.e. ['eur_usd', 'gbp_usd']).
+    :param year: string of the year to be analyzed (i.e. '2016').
+    :return: None -- The function saves the data in a file and does not return
+     a value.
+    """
+
+    # Parallel computing
+    with mp.Pool(processes=mp.cpu_count()) as pool:
+
+        # Basic functions
+        pool.starmap(gain_data_analysis_extraction
+                     .gain_fx_year_data_extraction,
+                     iprod(fx_pairs, [year]))
+        pool.starmap(gain_data_analysis_extraction
+                     .gain_fx_midpoint_year_data_extraction,
+                     iprod(fx_pairs, [year]))
+        pool.starmap(gain_data_analysis_extraction
+                     .gain_fx_trade_signs_year_data_extraction,
+                     iprod(fx_pairs, [year]))
+
+        # Plot
+        pool.starmap(gain_data_plot_extraction
+                     .gain_fx_quotes_year_plot,
+                     iprod(fx_pairs, [year]))
+        pool.starmap(gain_data_plot_extraction
+                     .gain_fx_midpoint_year_plot,
+                     iprod(fx_pairs, [year]))
+        pool.starmap(gain_data_plot_extraction
+                     .gain_fx_spread_year_plot,
+                     iprod(fx_pairs, [year]))
+
+    return None
+
+# -----------------------------------------------------------------------------
+
+
+def main():
+    """The main function of the script.
+
+    The main function extract, analyze and plot the data.
+
+    :return: None.
+    """
+
+    # Tickers and days to analyze
+    fx_pairs = ['eur_usd']
+    year = '2016'
+
+    # Run analysis
+    gain_data_plot_generator(fx_pairs, year)
+
+    print('Ay vamos!!')
+
+    return None
+
+# -----------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    main()
