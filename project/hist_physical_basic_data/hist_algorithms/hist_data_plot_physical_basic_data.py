42f89c9e7348838170fb0c9a59763d2be41b75e5
juanhenao21 <juanhenao21@gmail.com>
2020-03-31 14:32:36 +0200

    Update HIST plot physical module

--- project/hist_physical_basic_data/hist_algorithms/hist_data_plot_physical_basic_data.py
+++ project/hist_physical_basic_data/hist_algorithms/hist_data_plot_physical_basic_data.py
@@ -1,7 +1,7 @@
 '''HIST data plot module.
 
 The functions in the module plot the data obtained in the
-hist_data_analysis_extraction module.
+hist_data_analysis_physical_basic_data module.
 
 This script requires the following modules:
     * gc
@@ -25,66 +25,7 @@ import gc
 from matplotlib import pyplot as plt
 import pickle
 
-import hist_data_tools_extraction
-
-# ----------------------------------------------------------------------------
-
-
-def hist_fx_quotes_year_plot(fx_pair, year, weeks):
-    """Plots the quotes price for a year.
-
-    :param fx_pair: string of the abbreviation of the forex pair to be analyzed
-     (i.e. 'eur_usd').
-    :param year: string of the year to be analyzed (i.e. '2016').
-    :return: None -- The function saves the plot in a file and does not return
-     a value.
-    """
-
-    try:
-        function_name = hist_fx_quotes_year_plot.__name__
-        hist_data_tools_extraction \
-            .hist_function_header_print_plot(function_name, fx_pair, year, '')
-        fx_pair_upper = fx_pair[:3].upper() + '/' + fx_pair[4:].upper()
-
-        figure = plt.figure(figsize=(16, 9))
-
-        for week in weeks:
-            # Load data
-            fx_data = pickle.load(open(
-                            f'../../hist_data/extraction_data_{year}/hist_fx'
-                            + f'_data_extraction/{fx_pair}/hist_fx_data'
-                            + f'_extraction_{fx_pair}_w{week}.pickle', 'rb'))
-
-            plt.plot(fx_data['Date'], fx_data['Bid'], 'g', linewidth=5,
-                     label='Bid')
-            plt.plot(fx_data['Date'], fx_data['Ask'], 'b', linewidth=5,
-                     label='Ask')
-
-        # plt.legend(loc='best', fontsize=25)
-        plt.title(f'HIST quotes price - {fx_pair_upper}', fontsize=40)
-        plt.xlabel(r'Time $[s]$', fontsize=35)
-        plt.ylabel(r'Quotes $[\$]$', fontsize=35)
-        plt.xticks(fontsize=25)
-        plt.yticks(fontsize=25)
-        plt.grid(True)
-        plt.tight_layout()
-
-        # Plotting
-        hist_data_tools_extraction \
-            .hist_save_plot(function_name, figure, fx_pair, year, '')
-
-        plt.close()
-        del fx_data
-        del figure
-        gc.collect()
-
-        return None
-
-    except FileNotFoundError as e:
-        print('No data')
-        print(e)
-        print()
-        return None
+import hist_data_tools_physical_basic_data
 
 # ----------------------------------------------------------------------------
 
@@ -101,7 +42,7 @@ def hist_fx_midpoint_year_plot(fx_pair, year, weeks):
 
     try:
         function_name = hist_fx_midpoint_year_plot.__name__
-        hist_data_tools_extraction \
+        hist_data_tools_physical_basic_data \
             .hist_function_header_print_plot(function_name, fx_pair, year, '')
         fx_pair_upper = fx_pair[:3].upper() + '/' + fx_pair[4:].upper()
 
@@ -110,12 +51,11 @@ def hist_fx_midpoint_year_plot(fx_pair, year, weeks):
         for week in weeks:
             # Load data
             fx_data = pickle.load(open(
-                            f'../../hist_data/extraction_data_{year}/hist_fx'
-                            + f'_data_extraction/{fx_pair}/hist_fx_data'
-                            + f'_extraction_{fx_pair}_w{week}.pickle', 'rb'))
+                f'../../hist_data/physical_basic_data_{year}/hist_fx_physical'
+                + f'_data/{fx_pair}/hist_fx_physical_data_{fx_pair}_w{week}'
+                + f'.pickle', 'rb'))
 
-            plt.plot(fx_data['Date'], (fx_data['Bid'] + fx_data['Ask']) / 2,
-                     'g', linewidth=5)
+            plt.plot(fx_data['Date'], fx_data['Midpoint'], 'g', linewidth=5)
 
         plt.title(f'HIST midpoint price - {fx_pair_upper}', fontsize=40)
         plt.xlabel(r'Time $[s]$', fontsize=35)
@@ -126,7 +66,7 @@ def hist_fx_midpoint_year_plot(fx_pair, year, weeks):
         plt.tight_layout()
 
         # Plotting
-        hist_data_tools_extraction \
+        hist_data_tools_physical_basic_data \
             .hist_save_plot(function_name, figure, fx_pair, year, '')
 
         plt.close()
@@ -141,63 +81,6 @@ def hist_fx_midpoint_year_plot(fx_pair, year, weeks):
         print(e)
         print()
         return None
-
-# ----------------------------------------------------------------------------
-
-
-def hist_fx_spread_year_plot(fx_pair, year, weeks):
-    """Plots the spread for a year.
-
-    :param fx_pair: string of the abbreviation of the forex pair to be analyzed
-     (i.e. 'eur_usd').
-    :param year: string of the year to be analyzed (i.e. '2016').
-    :return: None -- The function saves the plot in a file and does not return
-     a value.
-    """
-
-    try:
-        function_name = hist_fx_spread_year_plot.__name__
-        hist_data_tools_extraction \
-            .hist_function_header_print_plot(function_name, fx_pair, year, '')
-        fx_pair_upper = fx_pair[:3].upper() + '/' + fx_pair[4:].upper()
-
-        figure = plt.figure(figsize=(16, 9))
-
-        for week in weeks:
-            # Load data
-            fx_data = pickle.load(open(
-                            f'../../hist_data/extraction_data_{year}/hist_fx'
-                            + f'_data_extraction/{fx_pair}/hist_fx_data'
-                            + f'_extraction_{fx_pair}_w{week}.pickle', 'rb'))
-
-            plt.plot(fx_data['Date'], fx_data['Ask'] - fx_data['Bid'], 'g',
-                     linewidth=5)
-
-        plt.title(f'HIST spread price - {fx_pair_upper}', fontsize=40)
-        plt.xlabel(r'Time $[s]$', fontsize=35)
-        plt.ylabel(r'Spread $[\$]$', fontsize=35)
-        plt.xticks(fontsize=25)
-        plt.yticks(fontsize=25)
-        plt.grid(True)
-        plt.tight_layout()
-
-        # Plotting
-        hist_data_tools_extraction \
-            .hist_save_plot(function_name, figure, fx_pair, year, '')
-
-        plt.close()
-        del fx_data
-        del figure
-        gc.collect()
-
-        return None
-
-    except FileNotFoundError as e:
-        print('No data')
-        print(e)
-        print()
-        return None
-
 
 # -----------------------------------------------------------------------------
 
