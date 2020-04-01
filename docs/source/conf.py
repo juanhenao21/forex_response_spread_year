2d0cad580e443ca56efcebd9ad2328da8f613f92
juanhenao21 <juanhenao21@gmail.com>
2020-04-01 15:48:31 +0200

    Add white line

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../../project/hist_download_data/hist_algorithms/'))
 sys.path.insert(0, os.path.abspath('../../project/hist_data_extraction/hist_algorithms/'))
 sys.path.insert(0, os.path.abspath('../../project/hist_responses_trade/hist_algorithms/'))
