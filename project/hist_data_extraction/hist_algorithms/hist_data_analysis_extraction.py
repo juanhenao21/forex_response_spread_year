Fix trade signs with zero values

project/hist_data_extraction/hist_algorithms/hist_data_analysis_extraction.py
@@ -231,6 +231,8 @@ def hist_fx_trade_signs_trade_data(fx_pair, year, week):
             else:
                 trade_signs[m_idx] = trade_signs[m_idx - 1]
 
+        if (trade_signs[0] == 0):
+            trade_signs[0] = trade_signs[1]
         assert np.sum(trade_signs == 0) == 0
 
         fx_data['Signs'] = trade_signs
@@ -259,7 +261,7 @@ def main():
     :return: None.
     """
 
-    hist_fx_data_extraction('eur_usd', '2019')
+    pass
 
     return None
 