a983cfc319aab6bf37ff8b293b246662464e6248
juanhenao21 <juanhenao21@gmail.com>
2020-04-03 11:23:36 +0200

    Update docstrings and parameters

--- project/hist_avg_spread/hist_algorithms/hist_data_analysis_avg_spread.py
+++ project/hist_avg_spread/hist_algorithms/hist_data_analysis_avg_spread.py
@@ -1,4 +1,4 @@
-'''TAQ data analysis module.
+'''HIST data analysis module.
 
 The functions in the module analyze the statistics from the NASDAQ stock
 market and compute the average spread of the stocks.
@@ -9,12 +9,12 @@ This script requires the following modules:
     * numpy
     * pandas
     * pickle
-    * taq_data_tools_avg_spread
+    * hist_data_tools_avg_spread
 
 The module contains the following functions:
-    * taq_quotes_trades_day_avg_spread_data - statistics of quotes and trades
+    * hist_quotes_trades_day_avg_spread_data - statistics of quotes and trades
       for a day.
-    * taq_quotes_trades_year_avg_spread_data - statistics of quotes and trades
+    * hist_quotes_trades_year_avg_spread_data - statistics of quotes and trades
       for a year.
     * main - the main function of the script.
 
@@ -28,22 +28,22 @@ from itertools import product as iprod
 import multiprocessing as mp
 import numpy as np
 import pandas as pd
+import pickle
 
-import taq_data_tools_avg_spread
+import hist_data_tools_avg_spread
 
 # ----------------------------------------------------------------------------
 
 
-def taq_quotes_trades_day_avg_spread_data(ticker, date):
-    """Obtain the quotes and trades statistics for a day.
+def hist_quotes_trades_day_avg_spread_data(fx_pair, week):
+    """Obtain the quotes and trades statistics for a week.
 
     Using the quotes files, obtain the statistics of the average spread, number
     of quotes and number of trades for a day.
 
-    :param ticker: string of the abbreviation of the stock to be analyzed
-     (i.e. 'AAPL').
-    :param date: string with the date of the data to be extracted
-     (i.e. '2008-01-02').
+    :param fx_pair: string of the abbreviation of the forex pair to be analyzed
+     (i.e. 'eur_usd').
+    :param week: string of the week to be analyzed (i.e. '16').
     :return: tuple -- The function returns a tuple with float values.
     """
 
@@ -55,82 +55,71 @@ def taq_quotes_trades_day_avg_spread_data(ticker, date):
 
     try:
         # Load data
-        data_quotes = pd.read_hdf(f'../../taq_data/hdf5_daily_data_{year}/taq'
-                              + f'_{ticker}_quotes_{date}.h5', key='/quotes',
-                              columns=['Bid', 'Ask'])
-        data_trades = pd.read_hdf(f'../../taq_data/hdf5_daily_data_{year}/taq'
-                              + f'_{ticker}_trades_{date}.h5', key='/trades',
-                              columns=['Ask'])
+        fx_data = pickle.load(open(
+                        f'../../hist_data/extraction_data_{year}/hist_fx'
+                        + f'_data_extraction/{fx_pair}/hist_fx_data_extraction'
+                        + f'_{fx_pair}_w{week}.pickle', 'rb'))
 
-        # Some files are corrupted, so there are some zero values that does not
-        # have sense
-        condition_quotes = data_quotes['Ask'] != 0
-        data_quotes = data_quotes[condition_quotes]
-        condition_trades = data_trades['Ask'] != 0
-        data_trades = data_trades[condition_trades]
+        spread = (fx_data['Ask'] - fx_data['Bid'])
 
-        spread = (data_quotes['Ask'] - data_quotes['Bid']) / 10000
-
-        num_quotes = len(data_quotes)
-        num_trades = len(data_trades)
+        num_quotes = len(spread)
         avg_spread = np.mean(spread)
 
-        return (num_quotes, num_trades, avg_spread)
+        return (num_quotes, avg_spread)
 
     except FileNotFoundError as e:
         print('No data')
         print(e)
         print()
-        return (np.NaN, np.NaN, np.NaN)
+        return (np.NaN, np.NaN)
 
 # ----------------------------------------------------------------------------
 
 
-def taq_quotes_trades_year_avg_spread_data(tickers, year):
+def hist_quotes_trades_year_avg_spread_data(fx_pairs, year):
     """Obtain the quotes and trades statistics for a year.
 
-    Using the taq_quotes_trades_day_avg_spread_data function computes the
+    Using the hist_quotes_trades_day_avg_spread_data function computes the
     statistics of the average spread, number of quotes and number of trades
     for a year.
 
-    :param tickers: list of the string abbreviation of the stocks to be
-     analyzed (i.e. ['AAPL', 'MSFT']).
-    :param year: string of the year to be analyzed (i.e '2016').
+    :param fx_pairs: list of the string abbreviation of the fx pairs to be
+     analyzed (i.e. ['eur_usd', 'gbp_usd']).
+    :param year: String of the years to be analyzed (i.e '2016').
     :return: None -- The function saves the data in a file and does not return
      a value.
     """
 
-    function_name = taq_quotes_trades_year_avg_spread_data.__name__
+    function_name = hist_quotes_trades_year_avg_spread_data.__name__
 
     # Pandas DataFrame to store the data
     spread_stats = pd.DataFrame(
-        columns=['Ticker', 'Avg_Quotes', 'Avg_Trades', 'Avg_Spread'])
+        columns=['FxPair', 'Avg_Quotes', 'Avg_Spread'])
 
-    dates = taq_data_tools_avg_spread.taq_bussiness_days(year)
+    weeks = hist_data_tools_avg_spread.hist_weeks()
 
-    for idx, ticker in enumerate(tickers):
+    for idx, fx_pair in enumerate(fx_pairs):
 
-        taq_data_tools_avg_spread \
-            .taq_function_header_print_data(function_name, ticker, ticker,
-                                             year, '', '')
+        hist_data_tools_avg_spread \
+            .hist_function_header_print_data(function_name, fx_pair, year, '')
 
         stat = []
-        args_prod = iprod([ticker], dates)
+        args_prod = iprod([fx_pair], weeks)
 
         # Parallel computation of the statistics. Every result is appended to
         # a list
         with mp.Pool(processes=mp.cpu_count()) as pool:
-            stat.append(pool.starmap(taq_quotes_trades_day_avg_spread_data,
+            stat.append(pool.starmap(hist_quotes_trades_day_avg_spread_data,
                         args_prod))
 
         # To obtain the average of the year, I average all the results of the
         # corresponding values (number quotes, trades and avg spread)
         stat_year = np.nanmean(stat[0], axis=0)
 
-        spread_stats.loc[idx] = [ticker] + list(stat_year)
+        spread_stats.loc[idx] = [fx_pair] + list(stat_year)
 
     spread_stats.sort_values(by='Avg_Spread', inplace=True)
-    spread_stats.to_csv(f'../taq_avg_spread_{year}.csv')
+    spread_stats.to_csv(f'../hist_avg_spread_{year}.csv')
     print(spread_stats)
 
     return None
