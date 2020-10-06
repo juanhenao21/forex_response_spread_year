b9714e7f5de10aea8c9a7e552fa22a265e2be6a4
juanhenao21 <juanhenao21@gmail.com>
2020-10-06 09:18:15 +0200

    Plot spread impact figure for the paper

--- paper/algorithms/05_spread_impact.py
+++ paper/algorithms/05_spread_impact.py
@@ -1,15 +1,16 @@
-'''TAQ spread impact figures
+'''HIST spread impact figures
 
-Plots the figures of the spread impact self-responses for the paper
+Plots the figures of the spread impact responses for the paper
 
 This script requires the following modules:
     * matplotlib
-    * numpy
     * pickle
+    * typing
+    * numpy
 
 The module contains the following functions:
-    * taq_self_response_year_avg_responses_physical_plot - plots the spread
-      impact responses for a year in physical scale.
+    * hist_response_year_avg_responses_plot - plots the spread impact
+      responses for a year.
     * main - the main function of the script.
 
 .. moduleauthor:: Juan Camilo Henao Londono <www.github.com/juanhenao21>
@@ -18,53 +19,81 @@ The module contains the following functions:
 # ----------------------------------------------------------------------------
 # Modules
 
-from matplotlib import pyplot as plt
-import numpy as np
 import pickle
+from typing import List
+
+from matplotlib import pyplot as plt  #type: ignore
+import numpy as np  #type: ignore
 
 # ----------------------------------------------------------------------------
 
-def taq_self_response_year_avg_responses_physical_plot(year):
-    """Plots the self-response average for a year.
+def hist_response_year_avg_responses_plot(years: List[str]) -> None:
+    """Plots the response average for different years.
 
-    :param ticker: string of the abbreviation of the stock to be analyzed
-     (i.e. 'AAPL').
-    :param year: string of the year to be analyzed (i.e '2008').
+    :param year: list of strings of the years to be analyzed
+     (i.e. ['2016', '2019']).
     :return: None -- The function saves the plot in a file and does not return
      a value.
     """
 
     try:
-        # Load data
-        resp_g1, resp_g2, resp_g3 = pickle.load(open(
-            f'../../project/taq_data/avg_responses_physical_data_{year}/taq'
-            + f'_self_response_year_avg_responses_physical_data/taq_self'
-            + f'_response_year_avg_responses_physical_data_{year}_.pickle',
-            'rb'))
+        figure, axs = plt.subplots(2, 3, sharex=True, figsize=(16, 9))
 
-        figure = plt.figure(figsize=(16, 9))
-        ax = plt.subplot(111)
+        year: str
+        for idx, year in enumerate(years):
 
-        plt.semilogx(resp_g1, linewidth=5, label=f'Group 1')
-        plt.semilogx(resp_g2, linewidth=5, label=f'Group 2')
-        plt.semilogx(resp_g3, linewidth=5, label=f'Group 3')
+            # Load data
+            resp_g1_t, resp_g2_t, resp_g3_t = pickle.load(open(
+                f'../../project/hist_data/avg_responses_data_{year}/hist_fx_self_response'
+                + f'_year_avg_responses_trade_data/hist_fx_self_response_year_avg'
+                + f'_responses_trade_data_{year}_.pickle', 'rb'))
+            resp_g1_p, resp_g2_p, resp_g3_p = pickle.load(open(
+                f'../../project/hist_data/avg_responses_data_{year}/hist_fx_self_response'
+                + f'_year_avg_responses_physical_data/hist_fx_self_response_year'
+                + f'_avg_responses_physical_data_{year}_.pickle', 'rb'))
 
-        plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=3,
-                   fontsize=30)
-        plt.xlabel(r'$\tau \, [s]$', fontsize=30)
-        plt.ylabel(r'$R^{\left(p\right)}_{ii}(\tau)$', fontsize=30)
-        plt.xticks(fontsize=20)
-        plt.yticks(fontsize=20)
-        plt.xlim(1, 1000)
-        plt.ylim(19.5 * 10 ** -5, 32.5 * 10 ** -5)
-        # plt.ylim(13 * 10 ** -5, 16 * 10 ** -5)
-        plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
-        ax.yaxis.offsetText.set_fontsize(20)
-        plt.grid(True)
+
+            axs[0, idx].semilogx(resp_g1_t, linewidth=5, label=f'Group 1')
+            axs[0, idx].semilogx(resp_g2_t, linewidth=5, label=f'Group 2')
+            axs[0, idx].semilogx(resp_g3_t, linewidth=5, label=f'Group 3')
+
+            # axs[idx, 0].legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=3,
+            #         fontsize=30)
+            axs[0, idx].set_title(f'{year}', fontsize=15)
+            axs[0, idx].set_xlabel(r'$\tau \, [trades]$', fontsize=13)
+            axs[0, idx].set_ylabel(r'$R^{\left(t\right)}_{ii}(\tau)$', fontsize=15)
+            axs[0, idx].tick_params(axis='x', labelsize=10)
+            axs[0, idx].tick_params(axis='y', labelsize=10)
+            axs[0, idx].set_xlim(1, 1000)
+            # axs[0, idx].ylim(19.5 * 10 ** -5, 32.5 * 10 ** -5)
+            axs[0, idx].ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+            axs[0, idx].yaxis.offsetText.set_fontsize(10)
+            axs[0, idx].grid(True)
+
+            axs[1, idx].semilogx(resp_g1_p, linewidth=5, label=f'Group 1')
+            axs[1, idx].semilogx(resp_g2_p, linewidth=5, label=f'Group 2')
+            axs[1, idx].semilogx(resp_g3_p, linewidth=5, label=f'Group 3')
+
+            # axs[1, idx].legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=3,
+            #                    fontsize=15)
+            # axs[1, idx].set_title(f'{year}', fontsize=15)
+            axs[1, idx].set_xlabel(r'$\tau \, [s]$', fontsize=13)
+            axs[1, idx].set_ylabel(r'$R^{\left(p\right)}_{ii}(\tau)$', fontsize=15)
+            axs[1, idx].tick_params(axis='x', labelsize=10)
+            axs[1, idx].tick_params(axis='y', labelsize=10)
+            axs[1, idx].set_xlim(1, 1000)
+            # axs[0, idx].ylim(19.5 * 10 ** -5, 32.5 * 10 ** -5)
+            axs[1, idx].ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+            axs[1, idx].yaxis.offsetText.set_fontsize(10)
+            axs[1, idx].grid(True)
+
+        axs[1, 1].legend(loc='upper center', bbox_to_anchor=(0.5, -0.2), ncol=3,
+                           fontsize=12)
+
         plt.tight_layout()
 
         # Save Plot
-        figure.savefig(f'../plot/06_spread_impact_{year}.png')
+        figure.savefig(f'../plot/05_spread_impact.png')
 
         return None
 
@@ -79,9 +108,9 @@ def taq_self_response_year_avg_responses_physical_plot(year):
 
 def main():
 
-    year = '2008'
+    years: List[str] = ['2011', '2015', '2019']
 
-    taq_self_response_year_avg_responses_physical_plot(year)
+    hist_response_year_avg_responses_plot(years)
 
     return None
 
