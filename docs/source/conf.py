5ad01cbd37b3905d111f75ba615695e8209a1505
juanhenao21 <juanhenao21@gmail.com>
2020-11-18 15:59:55 +0100

    Update docs conf files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,8 +44,8 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    #'sphinx.ext.autodoc',
-    'autoapi.extension',
+    'sphinx.ext.autodoc',
+    # 'autoapi.extension',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
@@ -53,7 +53,7 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
-autoapi_dirs = ['../../project']
+# autoapi_dirs = ['../../project']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -88,8 +88,8 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-from jupyter_sphinx_theme import *
-init_theme()
+# from jupyter_sphinx_theme import *
+# init_theme()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -100,7 +100,7 @@ init_theme()
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
